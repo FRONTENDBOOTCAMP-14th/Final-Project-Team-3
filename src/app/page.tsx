48e8521205022997ed6edf7957ab453fbc61d206
JYJ7435 <ju7435@naver.com>
2025-09-25 18:58:35 +0900

    [init] CSS, Supabase 연동
    - _theme.css에 BreakPoint, 공통 스타일 코드 수정 및 추가
    - Supabase DB 연동
    - 소셜로그인 테스트 (Google, Kakao)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import LoginTestButton from '../components/login-test'
+
 export default function HomePage() {
   return (
     <section>
       <p>홈</p>
+      <LoginTestButton />
     </section>
   )
 }
---
+++ src/libs/supabase/index.ts
@@ -0,0 +1,24 @@
+import { createClient } from '@supabase/supabase-js'
+import type {
+  Database,
+  Tables,
+  TablesInsert,
+  TablesUpdate,
+} from './database.types'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAPIKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY
+
+const supabase = createClient<Database>(supabaseUrl, supabaseAPIKey)
+
+export default supabase
+
+// 프로필 타입
+export type Profile = Tables<'profile'>
+export type ProfileInsert = TablesInsert<'profile'>
+export type ProfileUpdate = TablesUpdate<'profile'>
+
+// 스터디 룸 타입
+export type StudyRoom = Tables<'study_room'>
+export type StudyRoomInsert = TablesInsert<'study_room'>
+export type StudyRoomUpdate = TablesUpdate<'study_room'>
