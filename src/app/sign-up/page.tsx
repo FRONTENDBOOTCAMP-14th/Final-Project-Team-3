dfae50d6038b2e345cb2977a64aafdbdfed05af0
ksh2998 <ksh2998@naver.com>
2025-10-10 11:46:46 +0900

    [feat] sign-up 페이지 작성 및 profilepage 서버 분리

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import SignUpForm from '@/components/sign-up/SignUpForm'
+
+export default function SignUpPage() {
+  return (
+    <main className="sign-up-wrapper">
+      <SignUpForm />
+    </main>
+  )
+}
